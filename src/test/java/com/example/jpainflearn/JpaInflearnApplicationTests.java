56d93b1a68ff2c275f16c99a9c48d55a8488b125
Jun_Q <junlee36520620@gmail.com>
2023-03-20 23:57:43 +0900

    Initial commit

---
+++ src/test/java/com/example/jpainflearn/JpaInflearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpainflearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaInflearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
